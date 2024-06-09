40bcf3899ea0d1ea1eb591228eb0d1173f3cd82b
Nikita790231 <nburtsev7922@gmail.com>
2024-06-09 12:08:47 +0300

    style: удалены стили по умолчанию для выполненных задач

--- src/Components/TaskTodo.jsx
+++ src/Components/TaskTodo.jsx
@@ -4,7 +4,7 @@ export const TaskTodo = (props) => {
 	return (
 		<div key={props.id}>
 		  <input type={"checkbox"} onClick={() => props.toggleTask(props.id)} defaultChecked= {props.status}/>
-			<p style={props.status ? {textDecoration:"line-through"} : {textDecoration: "none"}}>{props.value}</p>
+			<p>{props.value}</p>
 			<button onClick={() => props.deleteTask(props.id)}>Удалить</button>
 		</div>
 	)
