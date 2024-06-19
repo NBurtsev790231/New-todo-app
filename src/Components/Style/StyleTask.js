7b53b10e663aaca93226dc3705d89f6d4bca34aa
NBurtsev790231 <nburtsev7922@gmail.com>
2024-06-19 12:02:01 +0300

    style: изменены стили поля ввода задач, добавлены первоначальные стили для вывода задач

--- src/Components/Style/StyleTask.js
+++ src/Components/Style/StyleTask.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyleInput = styled.div`
+export const StyleTask = styled.div`
 display: flex;
 flex-direction: row;
 justify-content: space-between;
@@ -10,16 +10,8 @@ width: 700px;
 height: 50px;
 border: 2px solid #000000;
 
-input {
-width: 500px;
-height: 35px;
-margin-left: 40px;
-border: 2px solid #6a5acd;
-border-radius: 10px;
-}
-
 button {
-	width: 100px;
+width: 100px;
 	height: 35px; 
 	font-size: 16px;
 	font-weight: 600;
@@ -28,4 +20,5 @@ button {
 	border-radius: 10px;
 	padding: 5px;
 	margin: 10px;
+}
 `
--- src/Components/TaskTodo.jsx
+++ src/Components/TaskTodo.jsx
@@ -1,11 +1,11 @@
-
+import { StyleTask } from "./Style/StyleTask";
 
 export const TaskTodo = (props) => {
 	return (
-		<div key={props.id}>
+		<StyleTask key={props.id}>
 		  <input type={"checkbox"} onClick={() => props.toggleTask(props.id)} defaultChecked= {props.status}/>
 			<p>{props.value}</p>
 			<button onClick={() => props.deleteTask(props.id)}>Удалить</button>
-		</div>
+		</StyleTask>
 	)
 };
