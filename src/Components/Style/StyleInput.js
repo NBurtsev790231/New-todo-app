151c1a6cd31da91a5506275b444a26a33ed5629b
NBurtsev790231 <nburtsev7922@gmail.com>
2024-06-19 11:42:29 +0300

    style: добавлены стили для поля ввода задач, стилизованна кнопка добавления задач на страницу

--- src/Components/Style/StyleInput.js
+++ src/Components/Style/StyleInput.js
@@ -1,12 +1,30 @@
 import styled from 'styled-components';
 
 export const StyleInput = styled.div`
-input {
-display: inline-box;
-margin-left: 100px;
-margin-right: 10px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+margin: 0px auto;
 width: 700px;
-height: 30px;
+height: 50px;
+border: 2px solid #000000;
+
+input {
+width: 500px;
+height: 35px;
+border: 2px solid #6a5acd;
+border-radius: 10px;
 }
 
+button {
+	width: 100px;
+	height: 35px; 
+	font-size: 16px;
+	font-weight: 600;
+	color: #ffffff;
+	background-color: #6a5acd; 
+	border-radius: 10px;
+	padding: 5px;
+	margin: 10px;
 `
